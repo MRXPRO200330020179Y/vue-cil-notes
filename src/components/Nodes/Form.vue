af00741aff2e3e9c4c3ae4950591dac6f1f4ccee
marat <mrxpro2003@gmail.com>
2022-06-27 12:01:36 +0500

    форма кушилди

---
+++ src/components/Nodes/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="note-from__wrapper">
+    <form class="note-form" @submit.prevent="onSubmit">
+        <textarea v-model="value" required placeholder="Type ur note"></textarea>
+        <button class="btn btnPrimary" type="submit">Add new note</button>
+    </form>
+	</div>
+</template>
+
+<style scoped> 
+.note-form{
+	display: flex;
+	flex-direction: column;
+	max-width: 600px;
+	width: 100%;
+}
+
+.note-from__wrapper{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+</style>
+
+<script>
+export default {
+	data(){
+		return{
+			value:''
+		}
+	},
+
+	methods:{
+		onSubmit(){
+			this.$emit('onSubmit', this.value)
+			this.value = ''
+		}
+	}
+}
+</script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Hello Home</h1>
+    <Form @onSubmit="handLeSubmit"></Form>
+    <List @onRemove="handLeRemove" :items="notes"></List>
 </template>
+
+<script>
+import Form from '@/components/Nodes/Form.vue';
+import List from '@/components/Nodes/List.vue';
+export default {
+    components:{
+        Form,
+        List
+    },
+
+    data(){
+        return{
+             notes:["task1", "task1", "task2"],
+        }
+    },
+
+    methods:{
+        handLeSubmit(note){
+            this.notes.push(note)
+        },
+
+        handLeRemove(index){
+            this.notes.splice(index,1);
+        }
+    }
+}
+</script>
